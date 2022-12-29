6653e61878a8c8030c9824e4ea79af7c85528732
Omar Lopez Guillen <omar.estudio36@gmail.com>
2022-12-29 15:43:53 +0000

    Commit v2, creamos tabla mail

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,11 +1,30 @@
 package kata5p1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Kata5P1 {
+
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:Kata5.db";
+        String sql = "CREATE TABLE IF NOT EXISTS mail (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " Mail text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         // TODO code application logic here
         SelectApp app = new SelectApp();
         app.selectAll();
+        createNewTable();
     }
-    
+
 }
